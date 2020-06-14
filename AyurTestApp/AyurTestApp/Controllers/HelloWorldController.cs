26f371d86d400c7ea2155cd253563616bb507726
kalpesht <kalpeshthakor1990@gmail.com>
2020-06-14 21:02:45 +0530

    First commit

---
+++ AyurTestApp/AyurTestApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AyurTestApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        static SqlConnection sqlConnHomeDAL;
+
+        public HelloWorldController()
+        {
+            sqlConnHomeDAL = new SqlConnection(Startup.ConnectionString);
+        }
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            int countdb = 0;
+            try
+            {
+                using (sqlConnHomeDAL = new SqlConnection(Startup.ConnectionString))
+                {
+                    string sqlQuery = "SELECT COUNT(*) FROM SYS.DATABASES";
+                    using (SqlCommand sqlCmd = new SqlCommand(sqlQuery, sqlConnHomeDAL))
+                    {
+                        sqlConnHomeDAL.Open();
+                        sqlCmd.CommandType = CommandType.Text;
+                        countdb= (int)sqlCmd.ExecuteScalar();
+                        sqlConnHomeDAL.Close();
+                    }
+                }
+                return "This is the Welcome action method..."+ "number of databases: "+ countdb.ToString();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+           
+        }
+    }
+}
---
+++ AyurTestApp/AyurTestApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AyurTestApp.Models;
+
+namespace AyurTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+
+
+            var text = "<h1>Hello, Response!</h1>";
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(text);
+            Response.Body.WriteAsync(data, 0, data.Length);
+            return View();
+            
+
+        }
+     
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
